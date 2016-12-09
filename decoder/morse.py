9c92001d2da78d0dbac261b51e398fb055c3f7e7
Hubertzhang <hubert.zyk@gmail.com>
2016-12-10 01:02:32 +0800

    update morse

--- decoder/morse.py
+++ decoder/morse.py
@@ -1,3 +1,5 @@
+import re
+
 morseAlphabet = {
     "A": ".-",
     "B": "-...",
@@ -39,32 +41,30 @@ morseAlphabet = {
 }
 
 inverseMorseAlphabet = dict((v, k) for (k, v) in morseAlphabet.items())
+validMorse = set([x for x in morseAlphabet.keys() if
+                  x.replace(".", "=").replace("-", ".").replace("=", "-") in inverseMorseAlphabet.keys()])
 
 
 # parse a morse code string positionInString is the starting point for decoding
-def decodeMorse(code="", positionInString=0):
-    if positionInString < len(code):
-        morseLetter = ""
-        for key, char in enumerate(code[positionInString:]):
-            if char == " ":
-                positionInString = key + positionInString + 1
-                letter = inverseMorseAlphabet[morseLetter]
-                return letter + decodeMorse(code, positionInString)
-
-            else:
-                morseLetter += char
-    else:
-        return ""
+def decodeMorse(code=""):
+    return ''.join([inverseMorseAlphabet[x] for x in re.split("[\\\s]", code)])
 
 
 # encode a message in morse code, spaces between words are represented by '/'
-def encodeToMorse(message=""):
-    encodedMessage = ""
-    for char in message[:]:
-        encodedMessage += morseAlphabet[char.upper()] + " "
-
-    return encodedMessage
+def encodeToMorse(message="", sep = " "):
+    return sep.join([morseAlphabet[x.upper()] for x in message])
 
 
-def reverseMorse(message=""):
+def flipByMorse(message=""):
+    return decodeMorse(flipByMorse(encodeToMorse(message)))
+
+
+def canBeReverse(message=""):
+    for x in message:
+        if x not in validMorse:
+            return False
+    return True
+
+
+def flipMorse(message=""):
     return message.replace(".", "=").replace("-", ".").replace("=", "-")
